ad6f1e935f06842da4865456a8a542501764a085
Lawal Damilare Toheeb <lawaltd088@gmail.com>
2023-06-28 20:00:32 +0100

    add mysql database

--- database.py
+++ database.py
@@ -6,30 +6,24 @@ engine = create_engine("mysql+pymysql://root:@localhost/empowerCo")
 
 def jobs_load_from_db():
     with engine.connect() as conn:
-      query = text('SELECT * FROM jobs')
-      results = conn.execute(query)
+        query = text('SELECT * FROM jobs')
+        results = conn.execute(query)
 
-      jobs = []
-      for row in results.all():
-        row_dict = row._asdict()
-        jobs.append(row_dict)
-      return(jobs)
-    
+        jobs = []
+        for row in results.all():
+            row_dict = row._asdict()
+            jobs.append(row_dict)
+        return (jobs)
+
+
 def load_job_from_db(id):
-   with engine.connect() as conn:
-      query = text('SELECT * FROM jobs WHERE id = :val')
-      results = conn.execute(query.params(val=id))
+    with engine.connect() as conn:
+        query = text('SELECT * FROM jobs WHERE id = :val')
+        results = conn.execute(query.params(val=id))
 
-      rows = results.all()
+        rows = results.all()
 
-      if len(rows) == 0:
-         return None
-      else:
-         return rows[0]
-
-   
-
-
-   
-    
-    
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0]._asdict()
